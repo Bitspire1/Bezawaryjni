49e887f1ef441f98724d83d25934537161311917
Bitspire1 <Bitspireone@proton.me>
2025-12-14 18:53:54 +0100

    dodanie robots.txt

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '7b0455f54324428c60e5e5acc0d5267703539248', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/kubap/Downloads/bezawaryjni/tina/__generated__/.cache/1765734096017', url: 'https://content.tinajs.io/2.0/content/78b60334-6683-472d-8d14-7efe074f8611/github/Nowy', token: '7b0455f54324428c60e5e5acc0d5267703539248', queries,  });
 export default client;
   
