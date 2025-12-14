b25d61b731410bdc17a4d082562d4775ad427e94
Bitspire1 <Bitspireone@proton.me>
2025-12-14 18:41:59 +0100

    fix: update tina branch to Nowszy

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tinacms';
 
 export default defineConfig({
-    branch: 'Nowy',
+    branch: 'Nowszy',
     clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "",
     token: process.env.TINA_TOKEN || "",
     build: {
