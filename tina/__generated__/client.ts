01ca4636ed157eea40392a863bf7603c902e6da7
Jakub Pujanek <48909@office.upsl.edu.pl>
2026-05-23 13:56:26 +0200

    tina chyba działa

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '7b0455f54324428c60e5e5acc0d5267703539248', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/kubap/Desktop/Bezawaryjni/tina/__generated__/.cache/1779537304810', url: 'https://content.tinajs.io/2.4/content/a24da33c-5968-465a-be46-1051703a6232/github/main', token: 'b7b71b150aa8a2bf7d72155fef67918b9c918ebe', queries,  });
 export default client;
   
